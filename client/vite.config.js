a9c20e21057ff2c1eb3e9a6582e91a6b6b670f3d
subham-kr-singh <subhamkumarsingh077@gmail.com>
2026-03-28 10:16:20 +0530

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://golf-charity-subscription-platform-bne5.onrender.com',
         changeOrigin: true,
       },
     },
